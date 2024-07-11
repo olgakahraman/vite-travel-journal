30298c56cb618eede9e3cd8b3ee004b7219a1704
olgakahraman <ol.khrmn@gmail.com>
2024-07-11 18:06:33 -0400

    created Navbar component and exported it into App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import Navbar from './components/Navbar'
 import './App.css'
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-    
+    <Navbar />
     </>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Navbar(){
+  return(
+    <nav>
+      <h5><i className="fa-solid fa-earth-americas"></i>my travel journal.</h5>
+    </nav>
+  )
+}
