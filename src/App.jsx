903c43b8519a24141fbc39036cc6de961192f8a7
olgakahraman <ol.khrmn@gmail.com>
2024-07-11 22:39:59 -0400

    exported navbar and card components into App.jsx and created a function with .map() to display card-list

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import './App.css'
+import React from 'react';
+import './App.css';
+import Card from './components/Card';
+import Navbar from './components/Navbar';
+import data from './data';
 
 function App() {
-  
+	const cards = data.map((item) => {
+		return <Card key={item.id} {...item} />;
+	});
 
-  return (
-    <>
-    <Navbar />
-    </>
-  )
+	return (
+		<div className="container">
+			<Navbar />
+			<section className="cards-list">{cards}</section>
+		</div>
+	);
 }
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Navbar(){
   return(
     <nav>
-      <h5><i className="fa-solid fa-earth-americas"></i>my travel journal.</h5>
+      <h5><i className="fa-solid fa-earth-americas"></i> my travel journal.</h5>
     </nav>
   )
 }
