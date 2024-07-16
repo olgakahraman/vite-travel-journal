fb0668a8f383e48d445009ea89e036091c61ab2d
olgakahraman <ol.khrmn@gmail.com>
2024-07-16 12:00:55 -0400

    created search input using useState from react and using .filter() to render search value

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
-import React from 'react';
+import { useState } from 'react';
 import './App.css';
 import Card from './components/Card';
 import Navbar from './components/Navbar';
+
 import data from './data';
 
 function App() {
-	const cards = data.map((item) => {
-		return <Card key={item.id} {...item} />;
-	});
+	const [search, setSearch] = useState('');
+
+	const cards = data
+		.filter((item) => {
+			return search.toLowerCase() === ''
+				? item
+				: item.title.toLowerCase().includes(search);
+		})
+		.map((item) => {
+			return <Card key={item.id} {...item} />;
+		});
 
 	return (
 		<div className="container">
 			<Navbar />
+			<input
+				type="text"
+				id="searchInput"
+				onChange={(e) => setSearch(e.target.value)}
+			/>
 			<section className="cards-list">{cards}</section>
 		</div>
 	);
